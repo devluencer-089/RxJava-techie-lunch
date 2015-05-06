50423dd9d219abb1253ec74ee6b51829205ad405
Michael Omann <michael.omann@gmail.com>
2015-05-06 23:53:48 +0200

    add new version of Wiki Exp

---
+++ src/main/java/com/senacor/rxjavatechielunch/WikiExp_WithWindowinh.java
@@ -0,0 +1,52 @@
+package com.senacor.rxjavatechielunch;
+
+import info.bliki.api.Page;
+import info.bliki.api.User;
+import org.apache.commons.lang3.StringUtils;
+import rx.Observable;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class WikiExp_WithWindowinh {
+    private final User user;
+
+    public WikiExp_WithWindowinh() {
+        user = new User("", "", "http://en.wikipedia.org/w/api.php");
+        user.login();
+    }
+
+    private void run() throws InterruptedException {
+        Observable<String> listOfTitleStrings = Observable.just("Goethe", "Schiller");
+
+        listOfTitleStrings
+                .window(50, TimeUnit.MILLISECONDS)
+                .flatMap(names -> queryWiki(names))
+                .flatMap(this::extractFullNamesFromRedirect)
+                .window(50, TimeUnit.MILLISECONDS)
+                .flatMap(fullName -> queryWiki(fullName))
+                .map(this::extractPersonInfoFromPage)
+                .subscribe(System.out::println);
+
+        Thread.sleep(5000);
+    }
+
+    private Observable<String> extractFullNamesFromRedirect(Page page) {
+        String currentContent = page.getCurrentContent();
+        return Observable.just(StringUtils.substringBetween(currentContent, "[[", "]]"));
+    }
+
+    private String extractPersonInfoFromPage(Page page) {
+        String currentContent = page.getCurrentContent();
+        return Arrays.stream(currentContent.split("\\n")).filter(line -> line.contains("|birth")).collect(Collectors.joining(" "));
+    }
+
+    private Observable<Page> queryWiki(Observable<String> titles) {
+        return Observable.from(user.queryContent(titles.toList().toBlocking().single()));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new WikiExp_WithWindowinh().run();
+    }
+}
