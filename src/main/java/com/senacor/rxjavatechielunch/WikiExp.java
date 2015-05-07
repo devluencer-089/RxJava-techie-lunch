cd128ea522dd89f69d72a4d0fe9330ded1995b16
unknown <tkreylin@KRETH-LT440sn.polaris.senacor.com>
2015-05-07 17:49:29 +0200

    extract service interface

--- src/main/java/com/senacor/rxjavatechielunch/WikiExp.java
+++ src/main/java/com/senacor/rxjavatechielunch/WikiExp.java
@@ -4,27 +4,32 @@ import info.bliki.api.Page;
 import info.bliki.api.User;
 import org.apache.commons.lang3.StringUtils;
 import rx.Observable;
+import rx.functions.Func1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class WikiExp {
     private final User user;
+    private final Func1<String, Observable<Page>> wikiService;
 
     public WikiExp() {
         user = new User("", "", "http://en.wikipedia.org/w/api.php");
         user.login();
+
+        wikiService = this::queryWiki;
     }
 
     private void run() throws InterruptedException {
         Observable<String> listOfTitleStrings = Observable.just("Goethe", "Schiller");
 
         listOfTitleStrings
-                .flatMap(name -> queryWiki(name))
+                .flatMap(wikiService)
                 .map(this::extractFullNamesFromRedirect)
-                .flatMap(fullName -> queryWiki(fullName))
+                .flatMap(wikiService)
                 .map(this::extractPersonInfoFromPage)
                 .subscribe(System.out::println);
 
